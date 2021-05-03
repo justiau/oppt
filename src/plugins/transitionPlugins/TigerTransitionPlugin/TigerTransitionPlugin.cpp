72208614c53b5fa155ff0aadcd830fba90dda6d9
justiau <justin.au14@gmail.com>
2021-05-03 12:21:19 +1000

    Checking pointing tiger cfg 2

--- src/plugins/transitionPlugins/TigerTransitionPlugin/TigerTransitionPlugin.cpp
+++ src/plugins/transitionPlugins/TigerTransitionPlugin/TigerTransitionPlugin.cpp
@@ -1,7 +1,6 @@
-
 #include "oppt/plugin/Plugin.hpp"
 #include "oppt/opptCore/Distribution.hpp"
-#include <iostream>
+// #include <iostream>
 
 namespace oppt
 {
@@ -15,17 +14,22 @@ public:
     virtual ~TigerTransitionPlugin() = default;
 
     virtual bool load(const std::string& optionsFile) override {
-        std::cout<<*optionsFile;
+        // std::cout<<*optionsFile;
+        debug::show_message("loading");
         return true;
     }
 
     virtual PropagationResultSharedPtr propagateState(const PropagationRequest* propagationRequest) const override {
         PropagationResultSharedPtr propagationResult(new PropagationResult());
-        return propagateResult;
+        VectorFloat actionVec = propagationRequest->action->as<VectorAction>()->asVector();
+        // std::cout<<actionVec;
+        debug::show_message("propagate state called");
+        VectorFloat resultingState(propagationRequest->currentState->as<VectorState>()->asVector());
+        return propagationResult;
     }
 
 };
 
-OPPT_REGISTER_TRANSITION_PLUGIN(DubinTransitionPlugin)
+OPPT_REGISTER_TRANSITION_PLUGIN(TigerTransitionPlugin)
 
 }
